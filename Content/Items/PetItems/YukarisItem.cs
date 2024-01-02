3d32fe9b1658b5f7fcfa0e127c69619c65fb3c95
chen xin <13854178862@qq.com>
2024-01-02 20:39:10 +0800

    为辉夜的满月光波加入一点特效 修复了美铃和咲夜的召唤物没法直接使用的问题 优化恋恋捅人机制 换个新封面

--- Content/Items/PetItems/YukarisItem.cs
+++ Content/Items/PetItems/YukarisItem.cs
@@ -17,5 +17,10 @@ namespace TouhouPets.Content.Items.PetItems
             Item.DefaultToVanitypet(ProjectileType<Yukari>(), BuffType<YukariBuff>());
             Item.DefaultToVanitypetExtra(30, 20);
         }
+        public override bool Shoot(Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback)
+        {
+            player.AddBuff(Item.buffType, 2);
+            return false;
+        }
     }
 }
