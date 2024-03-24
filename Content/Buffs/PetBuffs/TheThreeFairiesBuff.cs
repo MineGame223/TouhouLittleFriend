d6e805f9b9a72a4dd95f4009c1bcc8864f8303b2
chen xin <13854178862@qq.com>
2024-03-24 12:35:23 +0800

    加入三合一召唤物，更新日志

--- Content/Buffs/PetBuffs/TheThreeFairiesBuff.cs
+++ Content/Buffs/PetBuffs/TheThreeFairiesBuff.cs
@@ -5,6 +5,7 @@ namespace TouhouPets.Content.Buffs.PetBuffs
 {
     public class TheThreeFairiesBuff : BasicPetBuff
     {
+        public override bool LightPet => true;
         public override void Update(Player player, ref int buffIndex)
         {
             player.SpawnPetAndSetBuffTime(buffIndex, ProjectileType<Sunny>());
---
+++ Content/Items/PetItems/LightsJewels.cs
@@ -0,0 +1,34 @@
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.DataStructures;
+using TouhouPets.Content.Buffs.PetBuffs;
+using TouhouPets.Content.Projectiles.Pets;
+
+namespace TouhouPets.Content.Items.PetItems
+{
+    public class LightsJewels : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            Item.SacrificeCountNeeded(1);
+        }
+        public override void SetDefaults()
+        {
+            Item.DefaultToVanitypet(ProjectileType<Sunny>(), BuffType<TheThreeFairiesBuff>());
+            Item.DefaultToVanitypetExtra(26, 36);
+        }
+        public override bool Shoot(Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback)
+        {
+            player.AddBuff(Item.buffType, 2);
+            return false;
+        }
+        public override void AddRecipes()
+        {
+            CreateRecipe()
+            .AddIngredient(ItemType<SunnyMilk>(), 1)
+            .AddIngredient(ItemType<LunaMoon>(), 1)
+            .AddIngredient(ItemType<StarSapphire>(), 1)
+            .Register();
+        }
+    }
+}
